18901dbec1df17dfc3676a24a8f669969574d75a
user name <you@example.com>
2026-03-04 11:06:30 +0530

    project update

--- beginner/src/App.jsx
+++ beginner/src/App.jsx
@@ -2,31 +2,17 @@ import { useState } from 'react'
 import {Link,Routes,Route} from 'react-router-dom'
 import Users from './Users'
 import Contact from './Project/Contact'
-
+import Todo from'./Todo/Todo'
 import Profile from './Context/Profile'
 import   Homi from './Context/Homi'
 import './App.css'
 import './index.css'
 const App = () => {
   return(
-    <div>
-      <nav>
-        
-        <Link to='/Homi'>Homi</Link>
-        <Link to='/Profile'>Profile</Link> 
-        <Link to='/contact'>Contact</Link>
-
-        
-        </nav>
-      <Routes>
-        <Route path='/Homi' element={<Homi/>}/>
-        <Route path='/Profile' element={<Profile/>}/>
-        <Route path='/contact' element={<Contact/>}/>
-        
-      </Routes>
     
-  
-    </div>
+    <>
+    <Todo />
+    </>
   )
 }
 export default App;
